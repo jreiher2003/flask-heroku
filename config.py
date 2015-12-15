8a152c0bdf1b5d0578c2cec2f22227bfa22dae86
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-15 17:35:23 -0500

    set up postgres locally

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class BaseConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = '\x82\x8aJ\xect\xd1\x14\xcc\xf1 \xbdC\xe5\x97h\xe8\xa1\xc5\t\x12\xe3\xeb\x83\x16'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # print SQLALCHEMY_DATABASE_URI
 
 
 class DevelopmentConfig(BaseConfig):

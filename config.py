61e75490240524e69860f216dbd8e083135020d7
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-15 14:51:49 -0500

    add comments to config

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import os
 
 # default config
 class BaseConfig(object):
-	""" common configs between local and production """
+    """ common configs between local and production """
     DEBUG = False
     SECRET_KEY = '\x82\x8aJ\xect\xd1\x14\xcc\xf1 \xbdC\xe5\x97h\xe8\xa1\xc5\t\x12\xe3\xeb\x83\x16'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class DevelopmentConfig(BaseConfig):
-	""" local environment settings """
+    """ local environment settings """
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     DEBUG = True
 
 
 class ProductionConfig(BaseConfig):
-	""" heroku production settings for a live environment """
+    """ heroku production settings for a live environment """
     DEBUG = False

560748c3340607d1ffd04dca4be75792609950d1
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-15 15:19:05 -0500

    configs

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ class BaseConfig(object):
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = '\x82\x8aJ\xect\xd1\x14\xcc\xf1 \xbdC\xe5\x97h\xe8\xa1\xc5\t\x12\xe3\xeb\x83\x16'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class DevelopmentConfig(BaseConfig):
     """ local environment settings """
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     DEBUG = True
 
 

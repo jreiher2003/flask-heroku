622d83211921d145795d2a1b591b03a0fa8204fe
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-15 09:53:11 -0500

    part 11 generate secret key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'my precious'
+    SECRET_KEY = '\x82\x8aJ\xect\xd1\x14\xcc\xf1 \xbdC\xe5\x97h\xe8\xa1\xc5\t\x12\xe3\xeb\x83\x16'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 

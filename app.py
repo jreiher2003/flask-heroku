c613d69b1dc1e2b9d91e490da9440d8c1b1122e8
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-16 20:37:25 -0500

    add blueprints to project

--- app.py
+++ app.py
@@ -1,38 +1,45 @@
-# import the Flask class from the flask module
-from flask import Flask, render_template, redirect, \
-    url_for, request, session, flash
-from functools import wraps
+#################
+#### imports ####
+#################
+
+from flask import Flask, flash, redirect, session, url_for, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.bcrypt import Bcrypt 
-import sqlite3
-
-# create the application object
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
-# config
+from functools import wraps
 import os
+
+################
+#### config ####
+################
+
+app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-print os.environ['APP_SETTINGS']
-
-
-# create the sqlalchemy object
 db = SQLAlchemy(app)
 
-# import db schema
 from models import *
+from project.users.views import users_blueprint
+
+# register our blueprints
+app.register_blueprint(users_blueprint)
+
+##########################
+#### helper functions ####
+##########################
 
 
-# login required decorator
-def login_required(f):
-    @wraps(f)
+def login_required(test):
+    @wraps(test)
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
-            return f(*args, **kwargs)
+            return test(*args, **kwargs)
         else:
             flash('You need to login first.')
-            return redirect(url_for('login'))
+            return redirect(url_for('users.login'))
     return wrap
 
+
+################
+#### routes ####
+################
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -48,31 +55,9 @@ def welcome():
     return render_template('welcome.html')  # render a template
 
 
-# route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if (request.form['username'] != 'admin') \
-                or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in.')
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+####################
+#### run server ####
+####################
 
-
-@app.route('/logout')
-@login_required
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out.')
-    return redirect(url_for('welcome'))
-
-
-
-
-# start the server with the 'run()' method
 if __name__ == '__main__':
     app.run()

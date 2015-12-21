ddb11442307f8fa6a0005c4cd696cde909588f70
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-21 09:38:28 -0500

    add flask-login to project

--- project/users/views.py
+++ project/users/views.py
@@ -1,10 +1,8 @@
 #################
 #### imports ####
 #################
-
-from flask import flash, redirect, render_template, request, \
-    session, url_for, Blueprint
-from functools import wraps
+from flask import flash, redirect, render_template, request, url_for, Blueprint
+from flask.ext.login import login_user,login_required, logout_user
 from forms import LoginForm
 from project.models import User, bcrypt
 
@@ -16,22 +14,6 @@ users_blueprint = Blueprint(
     'users', __name__,
     template_folder='templates'
 )
-
-##########################
-#### helper functions ####
-##########################
-
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('users.login'))
-    return wrap
-
 
 ################
 #### routes ####
@@ -47,7 +29,8 @@ def login():
             if user is not None and bcrypt.check_password_hash(
                 user.password, request.form['password']
             ):
-                session['logged_in'] = True
+                # session['logged_in'] = True
+                login_user(user)
                 flash('You were logged in. Go Crazy.')
                 return redirect(url_for('home.home'))
 
@@ -59,6 +42,6 @@ def login():
 @users_blueprint.route('/logout')
 @login_required
 def logout():
-    session.pop('logged_in', None)
+    logout_user()
     flash('You were logged out.')
     return redirect(url_for('home.welcome'))

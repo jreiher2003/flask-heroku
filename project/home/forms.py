77a4f3fe3b524d9a3c3e857dd69db79579ccfb2e
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-21 14:15:25 -0500

    add mesaging system to project

---
+++ project/home/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import TextField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+class MessageForm(Form):
+    title = TextField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(max=140)])
+
+
--- project/home/views.py
+++ project/home/views.py
@@ -1,11 +1,13 @@
 #################
 #### imports ####
 #################
-from flask import render_template, Blueprint
-from flask.ext.login import login_required
 
 from project import db
-from project.models import BlogPost
+from project.models import BlogPost, User
+from forms import MessageForm
+from flask import render_template, Blueprint, request, flash, redirect, url_for
+from flask.ext.login import login_required, current_user
+
 
 ################
 #### config ####
@@ -15,17 +17,32 @@ home_blueprint = Blueprint(
     'home', __name__,
     template_folder='templates'
 )
+
+
 ################
 #### routes ####
 ################
 
 # use decorators to link the function to a url
-@home_blueprint.route('/')
+@home_blueprint.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    # return "Hello, World!"  # return a string
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)  # render a template
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('home.home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template(
+            'index.html', posts=posts, form=form, error=error)
 
 
 @home_blueprint.route('/welcome')
